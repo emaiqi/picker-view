32f28d92d4c0b2fa1034f96ffbc44861d636fc0e
洪海涛 <=>
2016-12-26 11:27:34 +0800

    城市选择器

--- pages/day/day.js
+++ pages/day/day.js
@@ -1,90 +1,61 @@
 var utils = require('../../utils/util');
-var currentProvince; // 滚轮当前指向的省
-var currentCity;
-var currentArea;
-
-
-
-const date = new Date();
-const years = [];
-const months = [];
-const days = [];
-
-const p=[];
-const a=[];
-const c=[];
-
-var riqi;
-var val;
-for (let i = 1990; i <= date.getFullYear(); i++) {
-    years.push(i)
-}
-
-for (let i = 1; i <= 12; i++) {
-    months.push(i)
-}
-
-for (let i = 1; i <= 31; i++) {
-    days.push(i)
-}
-
+const p = [];//省
+const a = [];//市
+const c = [];//区，县
+var riqi;//日期
+var val;//下标
 Page({
-    data: {
-        years: years,
-        year: date.getFullYear(),
-        months: months,
-        month: 2,
-        days: days,
-        day: 2
-    },
-    onLoad: function (options) {
-	    var dataC=utils.getData();
-	    // p.push(dataC.p);//province
-	    a.push(dataC.a);//area
-	    c.push(dataC.c);//city
-
-	    for (let i = 0; i < dataC.p.length; i++) {
-		    p.push(dataC.p);
-	    }
-
-	    console.log(p[0]);
-    },
-    bindChange: function (e) {
-        val = e.detail.value;
-        riqi = this.data;
-        /*this.setData({
-            year: this.data.years[val[0]],
-            month: this.data.months[val[1]],
-            day: this.data.days[val[2]]
-        })*/
-    },
-	yes:function () {
-		if (!currentProvince) {
-			currentProvince = p[0];
-			currentCity = c[currentProvince][0];
-			currentArea = a[currentCity][0];
-		}
-		this.setData({
-			showOrHide: 'show',
-			years: p[0],
-			months: c[0][currentProvince],
-			days: a[0][currentCity]
-		})
+	data: {
 	},
-    enter: function () {
-    	if(typeof(riqi)=="undefined" ){
-		    this.setData({
-			    nian: 1994,
-			    yue: 1,
-			    ri: 1
-		    })
-	    }else {
-		    this.setData({
-			    nian: riqi.year,
-			    yue: riqi.month,
-			    ri: riqi.day
-		    })
-	    }
-
-    }
+	onLoad: function (options) {
+		var dataC = utils.getData();
+		p.push(dataC.p);//province
+		a.push(dataC.a);//area
+		c.push(dataC.c);//city
+		this.setData({
+			years: p[0],
+			months: c[0][p[0][0]],
+			days: a[0][c[0][p[0][0]][0]]
+		})
+		console.log(c[0][p[0][0]][0]);
+	},
+	bindChange: function (e) {
+		val = e.detail.value;
+		riqi = this.data;
+		console.log(riqi.years[val[1]]);
+		this.setData({
+			months:c[0][riqi.years[val[0]]],
+			days:a[0][riqi.months[val[1]]]
+		 })
+	},
+	yes: function () {//获取城市信息
+		if (typeof(riqi) == "undefined") {
+			this.setData({
+				nian: "黑龙江省",
+				yue:"大兴安岭地区",
+				ri: "塔河县"
+			})
+		} else {
+			this.setData({
+				nian: this.data.years[val[0]],
+				yue: this.data.months[val[1]],
+				ri : this.data.days[val[2]]
+			})
+		}
+	},
+	enter: function () {
+		if (typeof(riqi) == "undefined") {
+			this.setData({
+				nian: "黑龙江省",
+				yue:"大兴安岭地区",
+				ri: "塔河县"
+			})
+		} else {
+			this.setData({
+				nian: this.data.years[val[0]],
+				yue: this.data.months[val[1]],
+				ri : this.data.days[val[2]]
+			})
+		}
+	}
 });
