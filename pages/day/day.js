a50e8462296d851d176c2c8757e91209ac0fdf63
洪海涛 <=>
2016-12-27 11:12:10 +0800

    修复显示隐藏的问题

--- pages/day/day.js
+++ pages/day/day.js
@@ -6,61 +6,68 @@ var riqi;//日期
 var val;//下标
 Page({
 	data: {
+		boxHide:"box-show",
+		showBox:"hide-show"
 	},
 	onLoad: function (options) {
 		var dataC = utils.getData();
 		p.push(dataC.p);//province
 		a.push(dataC.a);//area
 		c.push(dataC.c);//city
-		this.setData({
-			years: p[0],
-			months: c[0][p[0][0]],
-			days: a[0][c[0][p[0][0]][0]]
-		})
-		console.log(c[0][p[0][0]][0]);
 	},
 	bindChange: function (e) {
 		val = e.detail.value;
 		riqi = this.data;
-		console.log(riqi.years[val[1]]);
 		this.setData({
-			months:c[0][riqi.years[val[0]]],
-			days:a[0][riqi.months[val[1]]]
-		 })
+			months: c[0][riqi.years[val[0]]],
+			days: a[0][riqi.months[val[1]]]
+		})
 	},
 	yes: function () {//获取城市信息
-		if (typeof(riqi) == "undefined") {
+		if (typeof (riqi) == "undefined") {
 			this.setData({
 				nian: "黑龙江省",
-				yue:"大兴安岭地区",
+				yue: "大兴安岭地区",
 				ri: "塔河县"
 			})
 		} else {
 			this.setData({
 				nian: this.data.years[val[0]],
 				yue: this.data.months[val[1]],
-				ri : this.data.days[val[2]]
+				ri: this.data.days[val[2]]
 			})
 		}
 	},
-	cancelPick:function () {
-
+	cancelPick: function () {
+		this.setData({
+			boxHide:"box-show",
+			showBox:"hide-show"
+		})
 	},
-	enterPick:function () {
+	enterPick: function () {
 
+		console.log(c[0][p[0][0]][0]);
+		this.setData({
+			boxHide:"box-hide",
+			showBox:"show-box",
+			years: p[0],
+			months: c[0][p[0][0]],
+			days: a[0][c[0][p[0][0]][0]]
+
+		})
 	},
 	enter: function () {
-		if (typeof(riqi) == "undefined") {
+		if (typeof (riqi) == "undefined") {
 			this.setData({
 				nian: "黑龙江省",
-				yue:"大兴安岭地区",
+				yue: "大兴安岭地区",
 				ri: "塔河县"
 			})
 		} else {
 			this.setData({
 				nian: this.data.years[val[0]],
 				yue: this.data.months[val[1]],
-				ri : this.data.days[val[2]]
+				ri: this.data.days[val[2]]
 			})
 		}
 	}
